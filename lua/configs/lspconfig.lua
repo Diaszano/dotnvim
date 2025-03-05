3837a74f43b03d1a3bb810ddbfe1b8fffe9af3b5
Diaszano <61257292+Diaszano@users.noreply.github.com>
2025-03-05 13:38:51 -0300

    feat: configured lsp for golang

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,22 +3,33 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.gopls.setup({
+  on_init = nvlsp.on_init,
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  cmd = {
+    "gopls"
+  },
+  filetypes = {
+    "go",
+    "gomod",
+    "gowork",
+    "gotmpl",
+  },
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+        unusedwrite = true,
+        unusedvariable = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+    },
+  },
+})
