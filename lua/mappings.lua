422f70f93dc3c75d31c70296b1f9d9273fd6740f
Diaszano <61257292+Diaszano@users.noreply.github.com>
2025-03-05 13:36:33 -0300

    feat: organized and added new mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,29 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
+-- Mappings General
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<C-s>", "<cmd> w <CR>", { desc = "General save file" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Debugging (DAP)
+map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add breakpoint at line" })
+map("n", "<leader>dus", function()
+  local widgets = require("dap.ui.widgets")
+  local sidebar = widgets.sidebar(widgets.scopes)
+  sidebar.open()
+end, { desc = "Open debugging sidebar" })
+
+-- DAP Go
+map("n", "<leader>dgt", function()
+  require("dap-go").debug_test()
+end, { desc = "Debug go test" })
+
+map("n", "<leader>dgl", function()
+  require("dap-go").debug_last()
+end, { desc = "Debug last go test" })
+
+-- Go Tags (Gopher)
+map("n", "<leader>gsj", "<cmd> GoTagAdd json <CR>", { desc = "Add json struct tags" })
+map("n", "<leader>gsy", "<cmd> GoTagAdd yaml <CR>", { desc = "Add yaml struct tags" })
